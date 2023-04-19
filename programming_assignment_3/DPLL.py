57d6ebdf53f45d9d09321054a206a3c4eebdc644
shenoy1011 <shenoy1011@gmail.com>
2023-04-19 18:30:49 -0500

    This version can conclude a clause is True even if there are unknown literals

--- programming_assignment_3/DPLL.py
+++ programming_assignment_3/DPLL.py
@@ -4,6 +4,8 @@ import sys
 uchFlag = False
 symbolSet = set()
 clauseSet = set()
+symbolList = []
+clauseList = []
 totalCalls = 0
 
 def printCommand():
@@ -13,18 +15,21 @@ def printCommand():
     print(' ')
 
 def processCNF():
+    global symbolList
+    global clauseList
+    global uchFlag
     filename = sys.argv[1]
     file = open(filename, 'r')
     for line in file:
         if (line[0] != '#'):
-            clauseSet.add(line)
+            clauseList.append(line.strip())
             symbols = line.split()
             for sym in symbols:
                 if sym[0] == '-':
                     sym = sym[1:]
-                if sym not in symbolSet:
+                if sym not in symbolList:
                     # model[sym] = 0
-                    symbolSet.add(sym)
+                    symbolList.append(sym)
 
     if sys.argv[-1] == '+UCH':
         uchFlag = True
@@ -35,17 +40,21 @@ def processCNF():
             continue
         if sym[0] == '-':
             sym = sym[1:]
-        if sym not in symbolSet:
+        if sym not in symbolList:
             # model[sym] = 0
-            symbolSet.add(sym)
-    print('Set of symbols:', symbolSet)
+            symbolList.append(sym)
+    '''symbolList = list(symbolSet)
+    clauseList = list(clauseSet)'''
+    #print('Set of symbols:', clauseList)
 
 def unitClauseHeuristic(clauses, model):
+    print('UCH Clauses:')
     for clause in clauses:
         falseLiteralCount = 0
         unknownLiteralList = []
         isClauseTrue = False
         symbols = clause.split()
+        print(symbols)
         for sym in symbols:
             if sym[0] == '-':
                 if sym[1:] in model and model[sym[1:]] == False:
@@ -54,7 +63,7 @@ def unitClauseHeuristic(clauses, model):
                 elif sym[1:] in model and model[sym[1:]] == True:
                     falseLiteralCount += 1
                 else:
-                    unknownLiteralList.append(sym[1:])
+                    unknownLiteralList.append(sym)
             else:
                 if sym in model and model[sym] == True:
                     isClauseTrue = True
@@ -65,7 +74,7 @@ def unitClauseHeuristic(clauses, model):
                     unknownLiteralList.append(sym)
         if isClauseTrue == False and falseLiteralCount == len(symbols) - 1:
             #model.update({unknownLiteralList[0]:True})
-            if sym[0] == '-':
+            if unknownLiteralList[0][0] == '-':
                 return (unknownLiteralList[0][1:], False)
             else:
                 return (unknownLiteralList[0], True)
@@ -76,24 +85,29 @@ def checkClauses(clauses, model):
     trueClauseCount = 0
     falseClauseCount = 0
 
+    print('checkClauses Clauses:')
     for clause in clauses:
         falseLiteralCount = 0
         symbols = clause.split()
+        print(symbols)
         for sym in symbols:
             if sym[0] == '-':
                 if sym[1:] in model and model[sym[1:]] == False:
                     trueClauseCount += 1
+                    print('True')
                     break
                 elif sym[1:] in model and model[sym[1:]] == True:
                     falseLiteralCount += 1
             else:
                 if sym in model and model[sym] == True:
                     trueClauseCount += 1
+                    print('True')
                     break
                 elif sym in model and model[sym] == False:
                     falseLiteralCount += 1
         if falseLiteralCount == len(symbols):
             falseClauseCount += 1   # Probably don't need this line
+            print('False')
             return 0
         
     if trueClauseCount == len(clauses):
@@ -101,41 +115,46 @@ def checkClauses(clauses, model):
     
     return -1
 
-def DPPL(clauses, symbols, model):
-    # print(model, len(model) == len(symbolSet))
-    '''
-    if (4th paramter != "root node"):
-        print(4th parameter)
-    '''
+def DPPL(clauses, symbols, model, action):
+    if (action != 'Root node'):
+        print(action)
+
     global totalCalls
     totalCalls += 1
-    checkClausesRes = checkClauses(clauses, model)
+    print(model)
+    checkClausesRes = checkClauses(list(clauses), dict(model))
 
     if checkClausesRes == 1:
-        print('Model:', model)
+        print('FINAL MODEL:', model)
         return True
     if checkClausesRes == 0:
         return False
     
     if uchFlag == True:
-        uchTuple = unitClauseHeuristic(clauses, model)
-        if not uchTuple:
+        uchTuple = unitClauseHeuristic(list(clauses), dict(model))
+        if uchTuple:
             symbols.remove(uchTuple[0])
-            return DPPL(clauses, set(symbols), dict(model) | {uchTuple[0]:uchTuple[1]})
+            actionStr = 'UCH: Added ' + str(uchTuple[0]) + ' : ' + str(uchTuple[1]) + ' to model'
+            return DPPL(list(clauses), list(symbols), dict(model) | {uchTuple[0]:uchTuple[1]}, actionStr)
     
-    sym = symbols.pop()
+    sym = symbols.pop(0)
     
-    return DPPL(clauses, set(symbols), dict(model) | {sym:True}) or DPPL(clauses, set(symbols), dict(model) | {sym:False})
+    actionStrTrue = 'Added ' + str(sym) + ' : True to model'
+    actionStrFalse = 'Added ' + str(sym) + ' : False to model'
+    return DPPL(list(clauses), list(symbols), dict(model) | {sym:True}, actionStrTrue) or DPPL(list(clauses), list(symbols), dict(model) | {sym:False}, actionStrFalse)
 
 def DPPLSat():
     # filename = sys.argv[1]
-    result = DPPL(clauseSet, symbolSet, {})
+    result = DPPL(list(clauseList), list(symbolList), {}, 'Root node')
     if result == False:
         print('Unsatisifiable')
     print(totalCalls)
 
 printCommand()
 processCNF()
+print('Symbol List:', symbolList)
+print('Clause List:', clauseList)
+# print('UCHFlag:', uchFlag)
 DPPLSat()
 '''print('Number of arguments: {}'.format(len(sys.argv)))
 print('Argument(s) passed: {}'.format(str(sys.argv)))'''
