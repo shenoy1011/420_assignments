0ac888e89f5a607ec23eba1113bdda9998a8b973
shenoy1011 <shenoy1011@gmail.com>
2023-04-22 00:55:27 -0500

    Draft submission

--- programming_assignment_3/DPLL.py
+++ programming_assignment_3/DPLL.py
@@ -49,13 +49,13 @@ def processCNF():
     #print('Set of symbols:', clauseList)
 
 def unitClauseHeuristic(clauses, model):
-    print('UCH Clauses:')
+    # print('UCH Clauses:')
     for clause in clauses:
         falseLiteralCount = 0
         unknownLiteralList = []
         isClauseTrue = False
         symbols = clause.split()
-        print(symbols)
+        # print(symbols)
         for sym in symbols:
             if sym[0] == '-':
                 if sym[1:] in model and model[sym[1:]] == False:
@@ -86,29 +86,29 @@ def checkClauses(clauses, model):
     trueClauseCount = 0
     falseClauseCount = 0
 
-    print('checkClauses Clauses:')
+    # print('checkClauses Clauses:')
     for clause in clauses:
         falseLiteralCount = 0
         symbols = clause.split()
-        print(symbols)
+        # print(symbols)
         for sym in symbols:
             if sym[0] == '-':
                 if sym[1:] in model and model[sym[1:]] == False:
                     trueClauseCount += 1
-                    print('True')
+                    # print('True')
                     break
                 elif sym[1:] in model and model[sym[1:]] == True:
                     falseLiteralCount += 1
             else:
                 if sym in model and model[sym] == True:
                     trueClauseCount += 1
-                    print('True')
+                    # print('True')
                     break
                 elif sym in model and model[sym] == False:
                     falseLiteralCount += 1
         if falseLiteralCount == len(symbols):
             falseClauseCount += 1   # Probably don't need this line
-            print('False')
+            # print('False')
             return 0
         
     if trueClauseCount == len(clauses):
@@ -116,32 +116,53 @@ def checkClauses(clauses, model):
     
     return -1
 
+def printFinalModel(model):
+    finalModel = {}
+    for symbol in symbolList:
+        if symbol not in model:
+            finalModel[symbol] = 0
+        elif model[symbol] == True:
+            finalModel[symbol] = 1
+        elif model[symbol] == False:
+            finalModel[symbol] = -1
+    print('FINAL MODEL:', finalModel)
+
+
 def DPPL(clauses, symbols, model, action):
     if (action != 'Root node'):
         print(action)
 
     global totalCalls
     totalCalls += 1
-    print(model)
+    # print(model)
     checkClausesRes = checkClauses(list(clauses), dict(model))
 
     if checkClausesRes == 1:
-        print('FINAL MODEL:', model)
+        print('')
+        printFinalModel(model)
+        print('Just the satisfied propositions:')
+        propStr = ''
+        for key in model:
+            if model[key] == True:
+                propStr += key
+                propStr += ' '
+        print(propStr)
         return True
     if checkClausesRes == 0:
+        print('Backtracking occured')
         return False
     
     if uchFlag == True:
         uchTuple = unitClauseHeuristic(list(clauses), dict(model))
         if uchTuple:
             symbols.remove(uchTuple[0])
-            actionStr = 'UCH: Added ' + str(uchTuple[0]) + ' : ' + str(uchTuple[1]) + ' to model'
+            actionStr = 'UCH: Forced assignment of ' + str(uchTuple[0]) + ' : ' + str(uchTuple[1]) + ' to model'
             return DPPL(list(clauses), list(symbols), dict(model) | {uchTuple[0]:uchTuple[1]}, actionStr)
     
     sym = symbols.pop(0)
     
-    actionStrTrue = 'Added ' + str(sym) + ' : True to model'
-    actionStrFalse = 'Added ' + str(sym) + ' : False to model'
+    actionStrTrue = 'Trying ' + str(sym) + ' : True to model'
+    actionStrFalse = 'Trying ' + str(sym) + ' : False to model'
     return DPPL(list(clauses), list(symbols), dict(model) | {sym:True}, actionStrTrue) or DPPL(list(clauses), list(symbols), dict(model) | {sym:False}, actionStrFalse)
 
 def DPPLSat():
@@ -149,12 +170,13 @@ def DPPLSat():
     result = DPPL(list(clauseList), list(symbolList), {}, 'Root node')
     if result == False:
         print('Unsatisifiable')
-    print(totalCalls)
+    print('Total DPLL calls:', totalCalls)
+    print('UCH =', str(uchFlag))
 
 printCommand()
 processCNF()
-print('Symbol List:', symbolList)
-print('Clause List:', clauseList)
+# print('Symbol List:', symbolList)
+# print('Clause List:', clauseList)
 # print('UCHFlag:', uchFlag)
 DPPLSat()
 '''print('Number of arguments: {}'.format(len(sys.argv)))
